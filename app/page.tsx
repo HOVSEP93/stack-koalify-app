59558fdf81b5384025547fd493e688788dc6b365
Hovsep93 <hovsepantranig@gmail.com>
2024-11-04 15:57:22 +1100

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div className="mt-10 content-center justify-center text-center">
+    <>
       <h1 className="text-3xl font-bold text-violet-500 ">HERE WE Go!</h1>
-    </div>
+    </>
   );
 };
 
